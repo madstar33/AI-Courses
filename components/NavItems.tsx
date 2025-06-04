17a829638194da78a77ae503c7cab132d8bb0768
SAS <3mirmoh3m3d@gmail.com>
2025-06-04 13:09:23 +0330

    initial

---
+++ components/NavItems.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const navItems = [
+	{label:'Home', href:'/'},
+	{label: 'Companions', href:'/companions'},
+	{label:'My journy', href: '/my-journey'},
+]
+
+const NavItems = () => {
+
+	//current page we are in
+  const pathname = usePathname();
+
+  return (
+	<nav className='flex items-center gap-4'>
+		{/* automaticly destractur label and href */}
+		{navItems.map(({label, href}) => (
+			<Link href={href} key={label} 
+			className={cn(pathname === href && 'text-primary font-semibold')}>
+				{label}
+			</Link>
+		))}
+	</nav>
+  )
+}
+
+export default NavItems
